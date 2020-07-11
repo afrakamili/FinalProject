9d8bb621dff845a2078d34fc18e73a21c76ab051
unknown <mas.ikak@gmail.com>
2020-07-11 11:18:07 +0700

    redesign halaman tanyajawab menambahkan fitur vote dan komentar tp belum ada kontrollernya

--- final_project/resources/views/page/tanyajawab.blade.php
+++ final_project/resources/views/page/tanyajawab.blade.php
@@ -12,7 +12,23 @@
            
             <div class="section-block">
                 <h3 class="question text"><i class="fas fa-question-circle"></i> {{$pertanyaan->isi}}</h3>
-                <div class="answer"> ditanyakan oleh: </div>
+                <div class="answer mb-3"> 
+                    <ul class="list-inline float-left px-2">
+                        <li class="list-inline-item"><a href="#" class="text-secondary"><i class="fa fa-user"></i> oleh :{{$pertanyaan->id_penanya}}</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-secondary"><i class="fa fa-comment"></i> beri komentar</a></li>
+                    </ul>
+                                            
+                    <ul class="list-inline float-right px-2">
+                        <li class="list-inline-item">Vote :{{$pertanyaan->votes}} </a></li>
+                        <li class="list-inline-item"><a href="#" class="text-secondary"><i class="fa fa-thumbs-up"></i> vote up</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-secondary"><i class="fa fa-thumbs-down"></i> vote down</a></li>
+                     </ul>
+                </div>
+                <br>
+                <h2 class="section-title"></h2>
+                <span class="bg-secondary text-white-50">{{$pertanyaan->created_at}}</span>
+
+                <div class="answer mb-3">
                 
                 <form method="POST" action="{{url('/jawaban')}}">
                     @csrf
@@ -31,7 +47,21 @@
                     <h2 class="question text"><i class=""></i> Jawaban</h2>
                     <div class="answer">{{$jawaban->jawaban}}</div>
                     <div class="section-title"></div>
-                    <div class="answer">dijawab oleh </div>
+                    <div class="answer mb-2">
+                        <ul class="list-inline float-left px-2">
+                            <li class="list-inline-item"><a href="#" class=""><i class="fa fa-user"></i> dijawab oleh : {{$jawaban->id_penjawab}}</a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="fa fa-comment"></i> beri komentar</a></li>
+                        </ul>
+                                                
+                        <ul class="list-inline float-right px-2">
+                            <li class="list-inline-item">Vote :{{$jawaban->votes}} </a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="fa fa-thumbs-up"></i> vote up</a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="fa fa-thumbs-down"></i> vote down</a></li>
+                        </ul>
+                    </div>
+                    <br>
+                    <h2 class="section-title"></h2>
+                    <span class="bg-info text-white-50">{{$jawaban->created_at}}</span>
                 </div>
             @endforeach
             
